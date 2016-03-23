e5ada68ec4c69d0c2e1c6db7b931cad77b9dc826
poslegm <poslegm@gmail.com>
2016-03-23 23:04:58 +0300

    Figures classes moved to separate file

--- lab2/main.py
+++ lab2/main.py
@@ -1,51 +1,7 @@
 import glfw
 import math
 from OpenGL.GL import *
-
-
-class Cube:
-    __sides = ((0, 1, 2, 3), (3, 2, 7, 6), (6, 7, 5, 4),
-               (4, 5, 1, 0), (1, 5, 7, 2), (4, 0, 3, 6))
-
-    __colors = ((0, 0, 1, 1), (0, 1, 1, 1), (1, 1, 1, 1),
-                (1, 1, 0, 1), (1, 0, 0, 1), (1, 0, 1, 1))
-
-    def __init__(self, center, size):
-        self.__center = center
-        coord = size / 2
-        self.__vertices = (
-            (coord, -coord, -coord), (coord, coord, -coord),
-            (-coord, coord, -coord), (-coord, -coord, -coord),
-            (coord, -coord, coord), (coord, coord, coord),
-            (-coord, -coord, coord), (-coord, coord, coord)
-        )
-
-    def draw(self, shift, fill, scale=1.0, angle_x=0, angle_y=0):
-        self.__center[0] += shift[0]
-        self.__center[1] += shift[1]
-
-        glLoadIdentity()
-        glPushMatrix()
-
-        glTranslatef(*self.__center)
-        glScalef(scale, scale, scale)
-        glRotatef(angle_x, 1, 0, 0)
-        glRotatef(angle_y, 0, 1, 0)
-
-        if fill:
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-        else:
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-
-        glBegin(GL_QUADS)
-        for i in range(len(self.__sides)):
-            if fill:
-                glColor4f(*self.__colors[i])
-            for vertex in self.__sides[i]:
-                glVertex3f(*self.__vertices[vertex])
-        glEnd()
-
-        glPopMatrix()
+from figures import Cube
 
 
 def key_callback(window, key, scancode, action, mods):
